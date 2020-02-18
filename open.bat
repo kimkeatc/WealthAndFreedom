f4e1918c8401a6dd3f2b38e9aac05d529bd5d746
Chin, Kim Keat <kim.keat.chin@outlook.com>
2020-02-19 02:18:21 +0800

    Added homework 18Feb2020 part 2

---
+++ open.bat
@@ -0,0 +1,4 @@
+start https://www.klsescreener.com/v2/charting/chart/1538
+start https://www.klsescreener.com/v2/charting/chart/5212
+start https://www.klsescreener.com/v2/charting/chart/5225
+start https://www.klsescreener.com/v2/charting/chart/5288
