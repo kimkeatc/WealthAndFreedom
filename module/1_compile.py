b9e8526aa23f6bc841ecf35de7541eed1d38001d
Chin, Kim Keat <kim.keat.chin@outlook.com>
2020-02-29 00:52:17 +0800

    Added homework 27 and 28 Feb

--- module/1_compile.py
+++ module/1_compile.py
@@ -2,7 +2,7 @@ import Utility
 import pandas
 import os
 
-basefolder = r'C:\Users\kimke\OneDrive\Documents\investment and trading\stocks and equity\sandbox\WealthAndFreedom\logs\2020-02-26\temp'
+basefolder = r'C:\Users\kimke\OneDrive\Documents\investment and trading\stocks and equity\sandbox\WealthAndFreedom\logs\2020-02-28\temp'
 
 df = pandas.DataFrame()
 for index, filename in enumerate(os.listdir(basefolder)):
--- open.bat
+++ open.bat
@@ -1,9 +1,7 @@
-start https://www.klsescreener.com/v2/charting/chart/7204
-start https://www.klsescreener.com/v2/charting/chart/5145
-start https://www.klsescreener.com/v2/charting/chart/0167
-start https://www.klsescreener.com/v2/charting/chart/4863
-start https://www.klsescreener.com/v2/charting/chart/5285
-start https://www.klsescreener.com/v2/charting/chart/5141
+start https://www.klsescreener.com/v2/charting/chart/0021
+start https://www.klsescreener.com/v2/charting/chart/7277
+start https://www.klsescreener.com/v2/charting/chart/5347
+start https://www.klsescreener.com/v2/charting/chart/7803
 start https://www.klsescreener.com/v2/charting/chart/5014
-start https://www.klsescreener.com/v2/charting/chart/0040
-start https://www.klsescreener.com/v2/charting/chart/0023
+start https://www.klsescreener.com/v2/charting/chart/1155
+start https://www.klsescreener.com/v2/charting/chart/5246
