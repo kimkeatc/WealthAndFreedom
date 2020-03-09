76f12db7a8735d64e96348bef457181205809c33
Chin, Kim Keat <kim.keat.chin@outlook.com>
2020-03-09 22:12:33 +0800

    Added homework 9March20

--- module/1_compile.py
+++ module/1_compile.py
@@ -2,7 +2,7 @@ import Utility
 import pandas
 import os
 
-basefolder = r'C:\Users\kimke\OneDrive\Documents\investment and trading\stocks and equity\sandbox\WealthAndFreedom\logs\2020-03-06\temp'
+basefolder = r'C:\Users\kimke\OneDrive\Documents\investment and trading\stocks and equity\sandbox\WealthAndFreedom\logs\2020-03-09\temp'
 
 df = pandas.DataFrame()
 for index, filename in enumerate(os.listdir(basefolder)):
