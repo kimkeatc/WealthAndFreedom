1a64c0610784f503759e00e8ab57dad0c02ad84e
Chin, Kim Keat <kim.keat.chin@outlook.com>
2020-03-19 21:06:41 +0800

    Added homework 19March

--- module/3_merging.py
+++ module/3_merging.py
@@ -3,7 +3,7 @@ import Utility
 import pandas
 import os
 
-src_basefolder = join(abspath(join(dirname(__file__), '..')), 'logs', '2020-03-18', 'temp')
+src_basefolder = join(abspath(join(dirname(__file__), '..')), 'logs', '2020-03-19', 'temp')
 dst_basefolder = join(abspath(join(dirname(__file__), '..')), 'data')
 # src_basefolder = r'C:\Users\kimke\OneDrive\Documents\investment and trading\stocks and equity\sandbox\WealthAndFreedom\logs\2020-03-17\temp'
 # dst_basefolder = r'C:\Users\kimke\OneDrive\Documents\investment and trading\stocks and equity\sandbox\WealthAndFreedom\data'
