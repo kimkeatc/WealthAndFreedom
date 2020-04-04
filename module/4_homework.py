924a174dad6ef2a9ebe6ef269ea610f318795b22
Chin, Kim Keat <kim.keat.chin@outlook.com>
2020-04-04 15:28:55 +0800

    Added homework 3April20

--- module/4_homework.py
+++ module/4_homework.py
@@ -3,7 +3,7 @@ import utility
 import pandas
 
 src_basefolderpath = join(abspath(join(dirname(__file__), '..')), 'logs',
-                          '2020-04-02', 'daily.xlsx')
+                          '2020-04-03', 'daily.xlsx')
 dst_basefolderpath = join(abspath(join(dirname(__file__), '..')), 'data')
 
 with open(join(dirname(__file__), 'monitor.txt'), 'r') as f:
