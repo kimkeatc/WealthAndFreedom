253914f8c64f1c2b20a05ddb99332a136d1a04f2
Chin, Kim Keat <kim.keat.chin@outlook.com>
2020-04-15 01:32:13 +0800

    Added homework 14April20

--- module/1_compile.py
+++ module/1_compile.py
@@ -4,7 +4,7 @@ import pandas
 import os
 
 basefolder = join(abspath(join(dirname(__file__), '..')), 'logs',
-                  '2020-04-13', 'temp')
+                  '2020-04-14', 'temp')
 
 df = pandas.DataFrame()
 for index, filename in enumerate(os.listdir(basefolder)):
