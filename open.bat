d7264f2956641e9acee4616593b467d6693baacc
Chin, Kim Keat <kim.keat.chin@outlook.com>
2020-04-23 21:34:18 +0800

    Added homework 23April2020

--- open.bat
+++ open.bat
@@ -1,8 +1,9 @@
-start http://www.klsescreener.com/v2/charting/chart/7235
-start http://www.klsescreener.com/v2/charting/chart/5095
-start http://www.klsescreener.com/v2/charting/chart/5253
-start http://www.klsescreener.com/v2/charting/chart/7022
-start http://www.klsescreener.com/v2/charting/chart/7155
-start http://www.klsescreener.com/v2/charting/chart/0183
-start http://www.klsescreener.com/v2/charting/chart/3182
-start http://www.klsescreener.com/v2/charting/chart/5183
+start http://www.klsescreener.com/v2/charting/chart/4197
+start http://www.klsescreener.com/v2/charting/chart/8397
+start http://www.klsescreener.com/v2/charting/chart/7106
+start http://www.klsescreener.com/v2/charting/chart/5665
+start http://www.klsescreener.com/v2/charting/chart/7676
+start http://www.klsescreener.com/v2/charting/chart/6888
+start http://www.klsescreener.com/v2/charting/chart/6637
+start http://www.klsescreener.com/v2/charting/chart/3816
+start http://www.klsescreener.com/v2/charting/chart/0035
